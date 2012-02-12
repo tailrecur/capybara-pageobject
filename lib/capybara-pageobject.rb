a28658ffb685c6d3e7546734f8d333f4d917fc26
dlewis <deepak.lewis@gmail.com>
2012-02-12 10:16:03 +0000

    added configure block and loaded website method into test

--- lib/capybara-pageobject.rb
+++ lib/capybara-pageobject.rb
@@ -10,5 +10,29 @@ module Capybara
     autoload :Attribute, 'capybara-pageobject/attribute'
     autoload :Action, 'capybara-pageobject/action'
     autoload :Website, 'capybara-pageobject/website'
+
+    class << self
+      def configure
+        yield self
+      end
+
+      def pages_file file
+        @pages_file = file
+      end
+
+      def current_website
+        @website ||= Capybara::PageObject::Website.new(page, @pages_file)
+      end
+    end
+
+    module IncludedMethods
+      def website
+        Capybara::PageObject.current_website
+      end
+    end
+
+    def included(base)
+      base.send(:include, Capybara::PageObject::IncludedMethods)
+    end
   end
 end
